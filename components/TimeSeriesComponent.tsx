f61f322e29cdd4d6796812d613bf74b629f6ad4c
Arie Arya <ariearya@gmail.com>
2021-09-25 22:31:33 +0100

    fix timeseries issue

--- components/TimeSeriesComponent.tsx
+++ components/TimeSeriesComponent.tsx
@@ -1,40 +1,25 @@
-import React, { Component } from "react";
-// import Chart from "react-apexcharts";
+import { useState } from "react";
+import Chart from "react-apexcharts";
 
-class TimeSeriesComponent extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            options: {
-                chart: {
-                    id: "basic-bar"
-                },
-                xaxis: {
-                    categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-                }
-            },
-            series: [{
-                name: "series-1",
-                data: [30, 40, 45, 50, 49, 60, 70, 91]
-            }]
-        }
-    }
-    render() {
-        return ( <div className = "app">
-                    <div className = "row">
-                        <div className = "mixed-chart" >
+const TimeSeriesComponent = () => {
+    const [options, setOptions] = useState()
+    const [series, setSeries] = useState()
 
-                        < Chart options = { this.state.options }
-                            series = { this.state.series }
-                            type = "bar"
-                            width = "500" 
-                        />
-                        </div> 
-                    </div> 
+    return (
+        <div className="app">
+            <div className="row">
+                <div className="mixed-chart" >
+
+                    < Chart options={options}
+                        series={series}
+                        type="bar"
+                        width="500"
+                    />
                 </div>
-        );
-    }
-
+            </div>
+        </div>
+    )
 }
+
 export default TimeSeriesComponent;
